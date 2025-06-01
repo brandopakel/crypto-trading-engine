8a6b96442c858df89650062e124b51fb3bb8c9b8
B P <bp@Mac.attlocal.net>
2025-05-31 17:37:54 -0700

    Updated main file with main function. Incorporated loop to allow multi strategy select.

--- main.py
+++ main.py
@@ -5,20 +5,32 @@ from utils.product_id_finder import product_id
 from utils.class_strategy_select import strategy_select
 from utils.plot import plot_strategy
 
+def main():
+    initial_recommendations()
 
-initial_recommendations()
+    id = product_id()
+    coin = Coin(id)
+    coin.get_candles()
+    coin_candles = coin.fetch_candles()
+    while coin_candles is not None:
+        strategy = strategy_select()
+        plottable_df = strategy.apply(coin_candles)
+        print(plottable_df)
+        plot = strategy.plot(plottable_df)
+        choice = input("\n\nDo you want to choose another strategy? (Y/N): ")
+        if choice.strip().lower() == 'y':
+            continue
+        elif choice.strip().lower() == 'n':
+            print("\nThank you for your time.\n")
+            break
 
-#coin = coin_selector()
+if __name__ == "__main__":
+    main()
 
-id = product_id()
-coin = Coin(id)
-coin.get_candles()
-coin_candles = coin.fetch_candles()
-strategy = strategy_select()
-plottable_df = strategy.apply(coin_candles)
-plot = strategy.plot(plottable_df)
 
 """"
+initial_recommendations()
+coin = coin_selector()
 while coin is not None:
     strategy_selector(coin)
     choice = input("\n\nDo you want to choose another strategy? (Y/N): ")
